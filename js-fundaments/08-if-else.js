6e89d6f4cb1aa62863c04ecf4918b3832cc4b152
mario-laserna <laserna.mario@gmail.com>
2026-04-05 17:11:42 -0500

    if-else

---
+++ js-fundaments/08-if-else.js
@@ -0,0 +1,13 @@
+// IF/ELSE
+
+const edad = 18;
+
+if(edad >= 19){
+    // codigo a ejecutar si la condicion es verdadera
+    console.log("Eres mayor de edad");
+} else if(edad === 18){
+    console.log("Tienes 18 años");
+}else{
+    // codigo a ejecutar si la condicion es falsa
+    console.log("No eres mayor de edad");
+}
