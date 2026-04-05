9981bddf68fb32e68ee8347cfad8918f3db42b4d
mario-laserna <laserna.mario@gmail.com>
2026-04-05 17:07:13 -0500

    operadores lógicos

---
+++ js-fundaments/07-logic.js
@@ -0,0 +1,9 @@
+// Operadores lógicos
+
+// AND (&&) OR (||) NOT (!)
+
+// AND (&&) - devuelve true si ambos operandos son verdaderos
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
